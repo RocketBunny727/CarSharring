fac1de4ebf2c26d4b9900d7ec3f5af60b84228d2
Семен Гузенков <guzenkovsk@gmail.com>
2024-11-18 22:32:23 +0700

    realise ui, exit button and add app icon

--- mainwindow.h
+++ mainwindow.h
@@ -17,6 +17,11 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_auto_list_button_clicked();
+
+    void on_exit_button_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
